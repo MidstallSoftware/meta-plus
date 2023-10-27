4e0292ecef7e2b5f659bd676c7adf8bbd02fbac2
Tristan Ross <tristan.ross@midstall.com>
2023-10-26 17:53:44 -0700

    feat: add vtable creation method

--- src/meta+.zig
+++ src/meta+.zig
@@ -1,2 +1,3 @@
 pub const enums = @import("meta+/enums.zig");
 pub const fields = @import("meta+/fields.zig");
+pub const structs = @import("meta+/structs.zig");
---
+++ src/meta+/structs.zig
@@ -0,0 +1,77 @@
+const std = @import("std");
+
+pub fn createVTable(comptime T: type) type {
+    comptime {
+        const decls = std.meta.declarations(T);
+
+        var count: usize = 0;
+        for (decls) |decl| {
+            const func = @field(T, decl.name);
+            const info = @typeInfo(func);
+            if (std.meta.activeTag(info) == .Fn) count += 1;
+
+            const funcInfo = info.Fn;
+            if (funcInfo.params.len < 1) continue;
+        }
+
+        var fields: [count]std.builtin.Type.StructField = undefined;
+
+        var i: usize = 0;
+        for (decls) |decl| {
+            const func = @field(T, decl.name);
+            const info = @typeInfo(func);
+            if (std.meta.activeTag(info) != .Fn) continue;
+
+            const funcInfo = info.Fn;
+            if (funcInfo.params.len < 1) continue;
+            var params: [funcInfo.params.len]std.builtin.Type.Fn.Param = undefined;
+
+            for (funcInfo.params, &params) |param, *p| {
+                p.* = .{
+                    .is_generic = param.is_generic,
+                    .is_noalias = param.is_noalias,
+                    .type = if (param.type == *T) *anyopaque else if (param.type == *const T) *const anyopaque else param.type,
+                };
+            }
+
+            fields[i] = .{
+                .name = decl.name,
+                .type = @Type(.{
+                    .Pointer = .{
+                        .size = .One,
+                        .is_const = true,
+                        .is_volatile = false,
+                        .alignment = 0,
+                        .address_space = .generic,
+                        .child = @Type(.{
+                            .Fn = .{
+                                .calling_convention = .Unspecified,
+                                .alignment = funcInfo.alignment,
+                                .is_generic = funcInfo.is_generic,
+                                .is_var_args = funcInfo.is_var_args,
+                                .return_type = funcInfo.return_type,
+                                .params = params,
+                            },
+                        }),
+                        .is_allowzero = false,
+                        .sentinel = null,
+                    },
+                }),
+                .default_value = null,
+                .is_comptime = false,
+                .alignment = 0,
+            };
+
+            i += 1;
+        }
+
+        return @Type(.{
+            .Struct = .{
+                .layout = .auto,
+                .fields = fields,
+                .decls = .{},
+                .is_tuple = false,
+            },
+        });
+    }
+}
