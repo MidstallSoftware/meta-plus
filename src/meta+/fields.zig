7c5bdba4fd9485adece6ed7760386ce13319d0b4
Tristan Ross <tristan.ross@midstall.com>
2023-10-27 12:44:44 -0700

    feat(fields): add types struct, of method, and method method

--- src/meta+/fields.zig
+++ src/meta+/fields.zig
@@ -1,17 +1,31 @@
 const std = @import("std");
 
-pub fn rename(comptime T: type, comptime needle: []const u8, comptime replacement: []const u8) type {
+const types = struct {
+    pub const enums = @import("enums/fields.zig");
+    pub const structs = @import("structs/fields.zig");
+};
+
+pub usingnamespace types;
+
+pub fn of(comptime T: type) type {
     return switch (@typeInfo(T)) {
-        .Enum => @import("enums/fields.zig").rename(T, needle, replacement),
-        .Struct => @import("structs/fields.zig").rename(T, needle, replacement),
-        else => |f| @compileError("Not supported: " + f),
+        .Enum => types.enums,
+        .Struct => types.structs,
+        else => |f| @compileError("Not supported: " + @tagName(f)),
     };
+}
+
+pub fn method(comptime T: type, comptime name: []const u8) @TypeOf(@field(of(T), name)) {
+    const m = of(T);
+    if (!@hasDecl(m, name)) @compileError("Not supported: " + @tagName(std.meta.activeTag(@typeInfo(T))));
+    return @field(m, name);
+}
+
+pub fn rename(comptime T: type, comptime needle: []const u8, comptime replacement: []const u8) type {
+    return method(T, "rename")(T, needle, replacement);
 }
 
 pub fn mix(comptime Super: type, comptime Extend: type) type {
     if (std.meta.activeTag(@typeInfo(Extend)) != std.meta.activeTag(@typeInfo(Super))) @compileError("Super and Extend must be same type");
-    return switch (@typeInfo(Super)) {
-        .Struct => @import("structs/fields.zig").mix(Super, Extend),
-        else => |f| @compileError("Not supported: " + f),
-    };
+    return method(Super, "mix")(Super, Extend);
 }
